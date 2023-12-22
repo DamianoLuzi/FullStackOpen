55d36453497dbd2527accce71ae74ed48e756a93
DamianoLuzi <luzi.dami03@gmail.com>
2023-12-22 23:56:42 +0100

    2.11 getting data from the server using axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
-import Persons from "./Persons";
-import Filter from "./Filter";
-import PersonForm from "./PersonForm";
+import { useState, useEffect } from 'react';
+import Persons from "./components/Persons";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() =>{
+    console.log("use effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("response ",response)
+        setPersons(response.data)
+      })
+  },[])  
   const handleFilterChange = (event) => {
     console.log(event.target.value)
     setFilter(event.target.value)
