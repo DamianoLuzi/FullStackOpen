0fd0e15bf43ebe0f9f5c539396fa26a56ce4074b
DamianoLuzi <luzi.dami03@gmail.com>
2023-12-22 09:39:03 +0100

    1.3 + 1.4 + 1.5

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -4,9 +4,9 @@ const Content = (props) => {
     let parts = props.parts
     return(
         <>
-          <Part part = {parts[0].part} exercises = {parts[0].exercises}/>
-          <Part part = {parts[0].part} exercises = {parts[0].exercises}/>
-          <Part part = {parts[0].part} exercises = {parts[0].exercises}/>
+          <Part part = {parts[0].name} exercises = {parts[0].exercises}/>
+          <Part part = {parts[1].name} exercises = {parts[1].exercises}/>
+          <Part part = {parts[2].name} exercises = {parts[2].exercises}/>
         </>
     );
 }
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,6 +1,8 @@
 const Total = (props) => {
+    let parts = props.parts
+    let total = parts.map(p => p.exercises).reduce((ex1,ex2) => ex1 + ex2, 0)
     return (
-        <p>Number of exercises {props.total}</p>
+        <p>Number of exercises {total}</p>
     );
 }
 
