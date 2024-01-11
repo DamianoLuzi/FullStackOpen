32f5fb02d704331ca1b5d062c5a6ec39c658c56f
DamianoLuzi <luzi.dami03@gmail.com>
2024-01-11 23:01:25 +0100

    4.13-4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -44,4 +44,24 @@ blogsRouter.get("/:id", async (request,response,next) => {
     } catch {error => next(error)}
 })
 
+blogsRouter.put('/:id', async (request, response, next) => {
+    console.log("request body",request.body)
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+		url: body.url,
+		likes: body.likes? body.likes : 0 ,
+    }
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+        response.json(updatedBlog)
+    } catch { error => next(error)}
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
 module.exports = blogsRouter
